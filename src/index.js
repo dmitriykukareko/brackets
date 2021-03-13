b8bc2c4195243b09f2e7104ab8ccd8e0b810edd0
Dmitriy Kukareko <dv.kukareko@gmail.com>
2021-03-13 23:18:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracket = [];
+  let arr = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+      if (str[i] === bracket[bracket.length - 1]) {
+        bracket.pop();
+      } else {
+        bracket.push(arr.get(str[i]));
+      }
+  }
+  
+  return bracket.length === 0;
 }
